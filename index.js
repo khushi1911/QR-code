dc5e8917f54713a46f300a4c2c3e068563473e38
khushi1911 <khushikaushik2030@gmail.com>
2026-06-26 17:40:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,6 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
     } else {
-      // Something else went wrong
     }
   });
